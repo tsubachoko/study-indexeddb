5feea3f8906d2c4682b42a1c96a7f8b2c14f3ba7
chizzo <57238328+tsubachoko@users.noreply.github.com>
2022-03-14 22:22:48 +0900

    サンプル

--- dexie/pages/index.vue
+++ dexie/pages/index.vue
@@ -3,7 +3,22 @@
 </template>
 
 <script>
+import Dexie from 'dexie'
+
 export default {
-  name: 'IndexPage'
+  mounted() {
+    const db = new Dexie('dexie')
+    db.version(4).stores({
+      people: 'id,name,age,func'
+    })
+    
+    db.open()
+      .then(() => {
+        db.people.add({name: 'John', age: 42})
+        db.people.add({name: 'Mary', age: 35})
+        db.people.add({name: 'Peter', age: 27})
+        return db.people.toArray()
+      })
+  },
 }
 </script>
